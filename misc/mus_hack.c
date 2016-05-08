ecaa2a50aa18e6c3dfff641d663049e714bebb82
alexey.lysiuk <alexey.lysiuk@gmail.com>
2016-05-08 09:50:35 +0300

    + one more missing definition from SDL/i_sound.c

--- misc/mus_hack.c
+++ misc/mus_hack.c
@@ -1,4 +1,9 @@
+
+// Missing definitions from SDL/i_sound.c
+
 int mus_fluidsynth_chorus;
 int mus_fluidsynth_reverb;
 int mus_fluidsynth_gain; // NSM  fine tune fluidsynth output level
 int mus_opl_gain; // NSM  fine tune OPL output level
+
+int use_experimental_music = -1;
